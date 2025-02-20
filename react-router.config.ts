c3b3d68a5755e9adf8575373d94056acbd70745b
Marek Zelinka <mzelinka17@gmail.com>
2025-02-20 16:46:59 +0100

    setup

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,9 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  future: {
+    unstable_optimizeDeps: true,
+    unstable_splitRouteModules: true,
+    unstable_viteEnvironmentApi: true,
+  },
 } satisfies Config;
