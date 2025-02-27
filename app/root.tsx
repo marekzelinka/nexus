f60360d2938bc89f0df9980bb0fd92e1ae742e12
Marek Zelinka <mzelinka17@gmail.com>
2025-02-27 11:48:30 +0100

    remove default error handling

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,5 @@
 import { HotkeysProvider } from "react-hotkeys-hook";
-import {
-  isRouteErrorResponse,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "react-router";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 import type { Route } from "./+types/root";
 import "./app.css";
 import { Toaster } from "./components/ui/sonner";
@@ -29,35 +22,6 @@ export async function loader({ request }: Route.LoaderArgs) {
   const session = await getAuthSession(request);
 
   return { session };
-}
-
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
-
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
-
-  return (
-    <main className="container mx-auto p-4 pt-16">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full overflow-x-auto p-4">
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
-  );
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
