4b9d8154776ee17c8889ac5ba9c4c9bde55cf8a2
Marek Zelinka <mzelinka17@gmail.com>
2025-03-05 14:30:48 +0100

    deps

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,8 +4,4 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  future: {
-    unstable_optimizeDeps: true,
-    unstable_splitRouteModules: true,
-  },
 } satisfies Config;
