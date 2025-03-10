6f344ca2fd0712023956bae7b61b35a572f9ef97
Marek Zelinka <mzelinka17@gmail.com>
2025-03-10 14:35:57 +0100

    create notes

---
+++ app/components/note-form.tsx
@@ -0,0 +1,63 @@
+import { LoaderIcon, PlusIcon } from "lucide-react";
+import { useEffect, useRef } from "react";
+import { useFetcher } from "react-router";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+
+export function NoteForm() {
+  const fetcher = useFetcher();
+
+  const isPending =
+    fetcher.state !== "idle" &&
+    fetcher.formData?.get("intent") === "create-note";
+
+  const formRef = useRef<HTMLFormElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    // Clear and focus the input after the first submit
+    if (fetcher.data && !isPending) {
+      formRef.current?.reset();
+      textareaRef.current?.focus();
+    }
+  }, [isPending]);
+
+  return (
+    <fetcher.Form ref={formRef} method="post">
+      <input type="hidden" name="intent" value="create-note" />
+      <fieldset
+        className="relative disabled:cursor-not-allowed disabled:opacity-50"
+        disabled={isPending}
+      >
+        <div className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring">
+          <Label htmlFor="content" className="sr-only">
+            Note
+          </Label>
+          <Textarea
+            ref={textareaRef}
+            name="content"
+            id="content"
+            required
+            placeholder="What would you like to add?"
+            className="resize-none border-0 p-3 shadow-none focus-visible:ring-0 disabled:opacity-100"
+          />
+          {/* Spacer element to match the height of the toolbar */}
+          <div className="py-2" aria-hidden>
+            <div className="h-7" />
+          </div>
+        </div>
+        <div className="absolute inset-x-0 bottom-0 flex px-3 py-2">
+          <Button variant="ghost" size="icon" className="ml-auto size-7">
+            {isPending ? (
+              <LoaderIcon aria-hidden className="animate-spin" />
+            ) : (
+              <PlusIcon aria-hidden />
+            )}
+            <span className="sr-only">Add note</span>
+          </Button>
+        </div>
+      </fieldset>
+    </fetcher.Form>
+  );
+}
--- app/routes/contact-notes.tsx
+++ app/routes/contact-notes.tsx
@@ -1,4 +1,82 @@
+import { data } from "react-router";
+import { NoteForm } from "~/components/note-form";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { db } from "~/lib/db.server";
+import { requireAuthSession } from "~/lib/session.server";
+import type { Route } from "./+types/contact-notes";
+
+export async function action({ request, params }: Route.ActionArgs) {
+  const session = await requireAuthSession(request);
+
+  const contact = await db.contact.findUnique({
+    select: { id: true },
+    where: { id: params.contactId, userId: session.user.id },
+  });
+  if (!contact) {
+    throw data(`No user with the username "${params.contactId}" exists`, {
+      status: 404,
+    });
+  }
+
+  const formData = await request.formData();
+
+  const intent = formData.get("intent");
+  switch (intent) {
+    case "create-note": {
+      const content = String(formData.get("content"));
+
+      await db.note.create({
+        select: { id: true },
+        data: { content, contact: { connect: { id: contact.id } } },
+      });
+
+      break;
+    }
+    // case "toggle-task-completion": {
+    //   const todoId = String(formData.get("id"));
+    //   const completed = String(formData.get("completed"));
+
+    //   await updateTodo(userId, todoId, {
+    //     completed: completed === "true",
+    //     completedAt: completed === "true" ? new Date() : undefined,
+    //   });
+
+    //   break;
+    // }
+    // case "save-task": {
+    //   const todoId = String(formData.get("id"));
+    //   const description = String(formData.get("description"));
+
+    //   await updateTodo(userId, todoId, {
+    //     description,
+    //   });
+
+    //   break;
+    // }
+    // case "delete-task": {
+    //   const id = String(formData.get("id"));
+
+    //   await deleteTodo(userId, id);
+
+    //   break;
+    // }
+    // case "clear-completed-tasks": {
+    //   await clearCompletedTodos(userId);
+
+    //   break;
+    // }
+    // case "delete-all-tasks": {
+    //   await deleteAllTodos(userId);
+
+    //   break;
+    // }
+    default: {
+      throw data(`Invalid/Missing intent: ${intent}`, { status: 400 });
+    }
+  }
+
+  return { ok: true };
+}
 
 export default function ContactNotes() {
   return (
@@ -6,7 +84,9 @@ export default function ContactNotes() {
       <CardHeader>
         <CardTitle>Notes</CardTitle>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <NoteForm />
+      </CardContent>
     </Card>
   );
 }
